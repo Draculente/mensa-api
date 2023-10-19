abca03023f885b94511db50f9592050663784e48
Malte Fischer <malte.fischer@stud.th-luebeck.de>
2023-10-19 12:40:38 +0200

    :bulb: Add comments

--- src/cache.ts
+++ src/cache.ts
@@ -6,16 +6,27 @@ export default class Cache<T> {
     private _data: Opt<T> = none;
     private fetcher: () => Promise<T>;
 
+    /**
+     * Creates a new Cache instance
+     * @param ttl The time the cache is valid in milliseconds. If you load the data after the ttl, the class will make a request using the fetcher.
+     * @param fetcher The method used to fetch the data.
+     */
     constructor(ttl: number, fetcher: () => Promise<T>) {
         this.ttl = ttl;
         this.fetcher = fetcher;
         this.lastUpdated = 0;
     }
 
+    /**
+     * Returns the last update date as a Date object.
+     */
     public get lastUpdateDate(): Date {
         return new Date(this.lastUpdated);
     }
 
+    /**
+     * Returns the data from the cache or loads it from the fetcher if the cache is invalid (e.g. after the ttl expired).
+     */
     public get data(): Promise<T> {
         return new Promise((resolve, reject) => {
             this._data.filter(c => this.lastUpdated > Date.now() - this.ttl).onBoth(
@@ -38,6 +49,10 @@ export default class Cache<T> {
         });
     }
 
+    /**
+     * Refreshes the data stored in the cache.
+     * @returns The data from the fetcher.
+     */
     public async loadData(): Promise<T> {
         try {
             const data = await this.fetcher();
--- src/param_parsers.ts
+++ src/param_parsers.ts
@@ -3,6 +3,11 @@ export enum LocationEnum {
     MH = 9,
 }
 
+/**
+ * Parses the location parameter to a LocationEnum.
+ * @param param The location parameter.
+ * @returns A LocationEnum.
+ */
 export function newLocationEnum(param?: string): LocationEnum {
     return param === "mh" ? LocationEnum.MH : LocationEnum.TH;
 }
@@ -15,6 +20,11 @@ const weekdays = [
     ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
 ]
 
+/**
+ * Parses the day parameter to the number of a weekday.
+ * @param dayString The day parameter.
+ * @returns The index of the weekday (starting with sun = 0).
+ */
 export function parseDay(dayString?: string): number {
     if (!dayString) return -1;
     const day = dayString.toLowerCase();
